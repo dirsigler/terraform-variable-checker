4e0c48352119366fff83527d8f35fc1f41e0c14a
relgisri <dennis@irsigler.cloud>
2021-07-06 22:35:25 +0200

    feat(): initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module tfvars-check
+
+go 1.16
+
+require github.com/hashicorp/terraform-config-inspect v0.0.0-20210625153042-09f34846faab
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/hashicorp/terraform-config-inspect/tfconfig"
+)
+
+func main() {
+	var dir string
+	flag.StringVar(&dir, "path", ".", "Path to the root Terraform module.")
+	flag.String("verbose", "", "Verbose output")
+
+	flag.Parse()
+
+	module, _ := tfconfig.LoadModule(dir)
+
+	//tfReqVersion := module.RequiredCore[0]
+	tfconstraints := module.Variables
+	for i, v := range tfconstraints {
+
+		d := &v.Default
+		if *d == nil {
+			fmt.Printf("The variable %v has no configured default Value!\n", i)
+		}
+		if flag.Lookup("verbose") == nil {
+			fmt.Printf("Here is everything: %v\n.", v )
+		}
+	}
+}

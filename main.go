0ebecb7e7a8efa5b4abee1eb1d3df1d6f418e6e7
relgisri <dennis@irsigler.cloud>
2021-07-09 23:13:56 +0200

    feat(): move to method

--- main.go
+++ main.go
@@ -3,24 +3,42 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 
 	"github.com/hashicorp/terraform-config-inspect/tfconfig"
 )
 
+type tfdetails struct {
+	VarDefault interface{}
+	VarPosFn string
+	VarPosLn int
+}
+
+func (t *tfdetails) GetVars(tfDir string) (err error) {
+	module, _ := tfconfig.LoadModule(tfDir)
+	
+	tfconstraints := module.Variables
+	for _, v := range tfconstraints {
+		t.VarDefault = v.Default
+		t.VarPosFn = v.Pos.Filename
+		t.VarPosLn = v.Pos.Line
+	}
+	return err
+}
+
+
 func main() {
 	var dir string
 	flag.StringVar(&dir, "path", ".", "Path to the root Terraform module.")
-
+	
 	flag.Parse()
-
-	module, _ := tfconfig.LoadModule(dir)
-
-	tfconstraints := module.Variables
-	for i, v := range tfconstraints {
-
-		d := &v.Default
-		if *d == nil {
-			fmt.Printf("The variable %v has no configured default Value!\n", i)
-		}
+	var t tfdetails
+	err := t.GetVars(dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	if t.VarDefault == nil {
+		fmt.Printf("Empty default for variable %v in File %v at Line %v", t.VarDefault, t.VarPosFn, t.VarPosLn)
 	}
 }
